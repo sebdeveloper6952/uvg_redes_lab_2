ffbca0c0ddc80f1334d06d14bf2a95fb616db4fd
Sebastian Arriola <sebdeveloper6952@gmail.com>
2020-08-03 12:52:13 -0600

    hamming class.\

--- client.py
+++ client.py
@@ -1,7 +1,4 @@
 import socket
-from bitarray import bitarray
-from random import randrange
-from random import seed, choice
 from hamming import Hamming
 
 # hostname servidor/receptor
@@ -12,85 +9,26 @@ PORT = 6969
 NOISE_RATE = 50
 # para reproducibilidad
 RANDOM_SEED = 5000
-# aplicar seed para random
-seed(RANDOM_SEED)
-
-def hamming_encode(string):
-    hamming = bitarray([False] * (len(string) * 12))
-    for i in range(len(string)):
-        encoded = bitarray()
-        encoded.frombytes(string[i].encode('ascii'))
-        offset = i * 12
-        hamming[offset + 2] = encoded[0]
-        hamming[offset + 4] = encoded[1]
-        hamming[offset + 5] = encoded[2]
-        hamming[offset + 6] = encoded[3]
-        hamming[offset + 8] = encoded[4]
-        hamming[offset + 9] = encoded[5]
-        hamming[offset + 10] = encoded[6]
-        hamming[offset + 11] = encoded[7]
-        hamming[offset + 0] = hamming[offset + 2] ^ hamming[offset + 4] ^ hamming[offset + 6] ^ hamming[offset + 8] ^ hamming[offset + 10]
-        hamming[offset + 1] = hamming[offset + 2] ^ hamming[offset + 5] ^ hamming[offset + 6] ^ hamming[offset + 9] ^ hamming[offset + 10]
-        hamming[offset + 3] = hamming[offset + 4] ^ hamming[offset + 5] ^ hamming[offset + 6] ^ hamming[offset + 11]
-        hamming[offset + 7] = hamming[offset + 8] ^ hamming[offset + 9] ^ hamming[offset + 10] ^ hamming[offset + 11]
-    
-    return hamming
-
-def hamming_calculate_parity_bits(hamming):
-    old_parity_bits = bitarray([hamming[0], hamming[1], hamming[3], hamming[7]])
-    hamming[0] = hamming[2] ^ hamming[4] ^ hamming[6] ^ hamming[8] ^ hamming[10]
-    hamming[1] = hamming[2] ^ hamming[5] ^ hamming[6] ^ hamming[9] ^ hamming[10]
-    hamming[3] = hamming[4] ^ hamming[5] ^ hamming[6] ^ hamming[11]
-    hamming[7] = hamming[8] ^ hamming[9] ^ hamming[10] ^ hamming[11]
-    new_parity_bits = bitarray([hamming[0], hamming[1], hamming[3], hamming[7]])
-    
-    return (old_parity_bits, new_parity_bits)
-
-# Decodifica un array de bits a cadena de caracteres ascii,
-# aplicando correccion si es necesario.
-def hamming_decode(binary, correct=True):
-    data = bitarray()
-    for i in range(0, len(binary), 12):
-        old, new = hamming_calculate_parity_bits(bitarray(binary[i:i + 12]))
-        
-        if correct and (old != new):
-            a, sum = 1, 0
-            for j in range(4):
-                if old[j] != new[j]:
-                    sum = sum + a
-                a = a << 1
-            binary[i + (sum - 1)] = not binary[i + (sum - 1)]
-        data.append(binary[i + 2])
-        data.append(binary[i + 4])
-        data.append(binary[i + 5])
-        data.append(binary[i + 6])
-        data.append(binary[i + 8])
-        data.append(binary[i + 9])
-        data.append(binary[i + 10])
-        data.append(binary[i + 11])
-    
-    return data.tobytes().decode('ascii')
-    
-
-
+# ****************************************** HAMMING *********************************************
 # CAPA: APLICACION
 msg = 'ola amigo!'
 # CAPA: VERIFICACION
-hamming_encoded = hamming_encode(msg)
+hamming_encoder = Hamming(data=msg, random_seed=RANDOM_SEED)
+hamming_encoder.encode()
+print(f'Codificado: {hamming_encoder.hamming_encoded}')
 
 # CAPA: AGREGAR RUIDO
-options = [2, 4, 5, 6, 7, 8, 9, 10, 11]
-for i in range(0, len(hamming_encoded), 12):
-    p = randrange(100)
-    if p < NOISE_RATE:
-        c = choice(options)
-        hamming_encoded[i + c] = not hamming_encoded[i + c]
+hamming_encoder.add_noise(rate=NOISE_RATE)
+print(f'Se agregaron {hamming_encoder.noise_count} errores.')
 
 # decodificacion
-hamming_with_noise = hamming_decode(hamming_encoded, correct=False)
-hamming_decoded = hamming_decode(hamming_encoded)
-print(f'With noise: {hamming_with_noise}')
-print(f'Corrected: {hamming_decoded}')
+hamming_decoder = Hamming(binary=hamming_encoder.hamming_encoded)
+hamming_decoder.decode(correct=False)
+decoded = hamming_decoder.hamming_decoded
+print(f'Decodificado: {decoded}, se corrigieron {hamming_decoder.error_count} errores.')
+hamming_decoder.decode()
+decoded = hamming_decoder.hamming_decoded
+print(f'Decodificado: {decoded}, se corrigieron {hamming_decoder.error_count} errores.')
 
     
 # CAPA: TRANSMISION
---
+++ hamming.py
@@ -0,0 +1,85 @@
+from bitarray import bitarray
+from random import seed, choice, randrange
+
+class Hamming:
+    def __init__(self, data=None, binary=None, random_seed=1):
+        self.original_data = data
+        self.hamming_encoded = binary
+        self.error_count = 0
+        self.noise_count = 0
+        seed(random_seed)
+
+    def encode(self):
+        l = len(self.original_data)
+        hamming = bitarray([False] * l * 12)
+        for i in range(l):
+            encoded = bitarray()
+            encoded.frombytes(self.original_data[i].encode('ascii'))
+            offset = i * 12
+            hamming[offset + 2] = encoded[0]
+            hamming[offset + 4] = encoded[1]
+            hamming[offset + 5] = encoded[2]
+            hamming[offset + 6] = encoded[3]
+            hamming[offset + 8] = encoded[4]
+            hamming[offset + 9] = encoded[5]
+            hamming[offset + 10] = encoded[6]
+            hamming[offset + 11] = encoded[7]
+            hamming[offset + 0] = hamming[offset + 2] ^ hamming[offset + 4] ^ hamming[offset + 6] ^ hamming[offset + 8] ^ hamming[offset + 10]
+            hamming[offset + 1] = hamming[offset + 2] ^ hamming[offset + 5] ^ hamming[offset + 6] ^ hamming[offset + 9] ^ hamming[offset + 10]
+            hamming[offset + 3] = hamming[offset + 4] ^ hamming[offset + 5] ^ hamming[offset + 6] ^ hamming[offset + 11]
+            hamming[offset + 7] = hamming[offset + 8] ^ hamming[offset + 9] ^ hamming[offset + 10] ^ hamming[offset + 11]
+        
+        self.hamming_encoded = hamming
+
+    def hamming_calculate_parity_bits(self, blockIndex):
+        hamming = self.hamming_encoded[blockIndex:blockIndex + 12]
+        old_parity_bits = bitarray([hamming[0], hamming[1], hamming[3], hamming[7]])
+        hamming[0] = hamming[2] ^ hamming[4] ^ hamming[6] ^ hamming[8] ^ hamming[10]
+        hamming[1] = hamming[2] ^ hamming[5] ^ hamming[6] ^ hamming[9] ^ hamming[10]
+        hamming[3] = hamming[4] ^ hamming[5] ^ hamming[6] ^ hamming[11]
+        hamming[7] = hamming[8] ^ hamming[9] ^ hamming[10] ^ hamming[11]
+        new_parity_bits = bitarray([hamming[0], hamming[1], hamming[3], hamming[7]])
+
+        self.hamming_encoded[blockIndex:blockIndex + 12] = hamming
+        self.old_parity_bits = old_parity_bits
+        self.new_parity_bits = new_parity_bits
+
+        return (old_parity_bits, new_parity_bits)
+    
+    def decode(self, correct=True):
+        if correct:
+            self.error_count = 0
+
+        data = bitarray()
+        for i in range(0, len(self.hamming_encoded), 12):
+            if correct:
+                old, new = self.hamming_calculate_parity_bits(i)
+                if old != new:
+                    self.error_count = self.error_count + 1
+                    a, sum = 1, 0
+                    for j in range(4):
+                        if old[j] != new[j]:
+                            sum = sum + a
+                        a = a << 1
+                    sum = sum - 1
+                    self.hamming_encoded[i + sum] = not self.hamming_encoded[i + sum]
+            
+            data.append(self.hamming_encoded[i + 2])
+            data.append(self.hamming_encoded[i + 4])
+            data.append(self.hamming_encoded[i + 5])
+            data.append(self.hamming_encoded[i + 6])
+            data.append(self.hamming_encoded[i + 8])
+            data.append(self.hamming_encoded[i + 9])
+            data.append(self.hamming_encoded[i + 10])
+            data.append(self.hamming_encoded[i + 11])
+        
+        self.hamming_decoded = data.tobytes().decode('ascii')
+    
+    def add_noise(self, rate=1):
+        options = [2, 4, 5, 6, 7, 8, 9, 10, 11]
+        for i in range(0, len(self.hamming_encoded), 12):
+            p = randrange(100)
+            if p < rate:
+                self.noise_count = self.noise_count + 1
+                c = choice(options)
+                self.hamming_encoded[i + c] = not self.hamming_encoded[i + c]
